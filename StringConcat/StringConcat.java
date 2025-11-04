126967995900ed7f0ce4e1a1b01fb43d316d8163
Stephen Kisengese <skisengese@gmail.com>
2025-11-04 06:24:07 +0300

    feat: check for either null

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,11 +1,9 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        if (s1 == null) {
-            s1 = "";
+        if (s1 == null || s2 == null) {
+            return null;
         }
-        if (s2 == null) {
-            s2 = "";
-        }
+        
         return s1.concat(s2);
     }
 }
