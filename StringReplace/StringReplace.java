e46014b81743aa7e19ac2142143b0f4dae724070
Stephen Kisengese <skisengese@gmail.com>
2025-11-04 06:42:43 +0300

    fix: parameter signature

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -1,6 +1,6 @@
 public class StringReplace {
-    public static String replace(String s, String target, String replacement) {
-        if (s == null || target == null || replacement == null) {
+    public static String replace(String s, char target, char replacement) {
+        if (s == null) {
             return s;
         }
         return s.replace(target, replacement);
