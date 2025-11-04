2dd1048859ca79613b14d19dd8db7474dad5d295
Stephen Kisengese <skisengese@gmail.com>
2025-11-04 06:30:52 +0300

    feat: add substring method

---
+++ StringContains/StringContains.java
@@ -0,0 +1,8 @@
+public class StringContains {
+    public static boolean isStringContainedIn(String subString, String s) {
+        if (subString == null || s == null) {
+            return false;
+        }
+        return s.contains(subString);
+    }
+}
