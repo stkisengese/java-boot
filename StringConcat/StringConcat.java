9914191d2fe3d972ba2616f97d80e54b6f30507d
Stephen Kisengese <skisengese@gmail.com>
2025-11-04 06:26:16 +0300

    fix: fix when an option is null

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,9 +1,15 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        if (s1 == null || s2 == null) {
+        if (s1 == null && s2 == null) {
             return null;
         }
-        
+        if (s1 == null) {
+            return s2;
+        }
+        if (s2 == null) {
+            return s1;
+        }
+
         return s1.concat(s2);
     }
 }
