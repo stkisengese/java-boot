d06d5faec4db056ede71ade62b8074edbba892c9
Stephen Kisengese <skisengese@gmail.com>
2025-11-04 05:53:41 +0300

    feat: add switch case

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,12 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        switch (chifoumiAction) {
+            case ROCK:
+                return ChifoumiAction.SCISSOR;
+            case PAPER:
+                return ChifoumiAction.ROCK;
+            case SCISSOR:
+                return ChifoumiAction.PAPER;
+        }
+    }
+}
