c20e8735742099b0ee80100c46498eaf21f86e24
Stephen Kisengese <skisengese@gmail.com>
2025-11-05 20:38:08 +0300

    fix: handle empty args to print newline

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -1,9 +1,5 @@
 public class SortArgs {
     public static void sort(String[] args) {
-        if (args == null || args.length == 0) {
-            return;
-        }
-
         // Convert String array to Integer array for sorting
         Integer[] intArgs = new Integer[args.length];
         for (int i = 0; i < args.length; i++) {
@@ -18,12 +14,14 @@ public class SortArgs {
         java.util.Arrays.sort(intArgs);
 
         // print numbers seperated by spaces, end in a newline
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < intArgs.length; i++) {
             if (i > 0) {
-                System.out.print(" ");  
+                sb.append(" ");
             }
-            System.out.print(intArgs[i]);
+            sb.append(intArgs[i]);
         }
-        System.out.println();
+        sb.append("\n");
+        System.out.print(sb.toString());
     }
 }
