473f8c4370552829cd3c718c4ed3d90415cdfb37
Stephen Kisengese <skisengese@gmail.com>
2025-11-05 06:34:49 +0300

    fix: standadize the string to lower case

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
         // Remove leading/trailing spaces
-        s = s.strip();
+        s = s.strip().toLowerCase();
        // s = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
 
         // Create a reversed version of the string
