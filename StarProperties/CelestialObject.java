43ab35b32daf921b0dc3862922b16cf533e29a88
Stephen Kisengese <skisengese@gmail.com>
2025-11-06 06:31:56 +0300

    feat: add constructors

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -3,4 +3,20 @@ public class CelestialObject {
     public double y;
     public double z;
     public String name;
+
+    // defautt constructor
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    // parameterized constructor
+    public CelestialObject(double x, double y, double z, String name) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
 }
