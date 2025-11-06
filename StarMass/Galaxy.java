f49138c8cca3c787fe5d9f47594206dd3202769d
Stephen Kisengese <skisengese@gmail.com>
2025-11-06 19:07:31 +0300

    feat: compute total map using HashMap

--- StarMass/Galaxy.java
+++ StarMass/Galaxy.java
@@ -1,8 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Galaxy {
-
     private List<CelestialObject> celestialObjects;
 
     // default constructor
@@ -20,4 +21,25 @@ public class Galaxy {
         return celestialObjects;
     }
 
+    // method to compute the mass of all objects in the galaxy
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> massMap = new HashMap<>();
+
+        // Initialize mass map
+        massMap.put("Star", 0);
+        massMap.put("Planet", 0);
+        massMap.put("Other", 0);
+
+        for (CelestialObject obj : celestialObjects) {
+            if (obj instanceof Star) {
+                massMap.put("Star", massMap.get("Star") + obj.getMass());
+            } else if (obj instanceof Planet) {
+                massMap.put("Planet", massMap.get("Planet") + obj.getMass());
+            } else {
+                massMap.put("Other", massMap.get("Other") + obj.getMass());
+            }
+        }
+        
+        return massMap;
+    }
 }
