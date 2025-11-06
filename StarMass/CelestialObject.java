b0982eeeb24804b3c5478f8293122441d80e0ecf
Stephen Kisengese <skisengese@gmail.com>
2025-11-06 18:15:10 +0300

    chore: rename file structure

--- StarMass/CelestialObject.java
+++ StarMass/CelestialObject.java
@@ -12,9 +12,6 @@ public class CelestialObject {
 
     // defautt constructor
     public CelestialObject() {
-        this.x = 0.0;
-        this.y = 0.0;
-        this.z = 0.0;
         this.name = "Soleil";
     }
 
