e081ded0325be165cf4b7829450b4e6691549160
Stephen Kisengese <skisengese@gmail.com>
2025-11-06 09:10:58 +0300

    chore: rename file

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -21,31 +21,15 @@ public class CelestialObject {
     }
 
     // getter methods
-    public double getX() {
-        return this.x;  
-    }
-    public double getY() {
-        return this.y;  
-    }
-    public double getZ() {
-        return this.z;
-    }  
-    public String getName() {
-        return this.name;
-    }
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
+    public double getZ() { return this.z; }  
+    public String getName() { return this.name; }
 
     // Setter methods
-    public void setX(double x) {
-        this.x = x;
-    }
-    public void setY(double y) {
-        this.y = y;
-    }
-    public void setZ(double z) {
-        this.z = z;
-    } 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setZ(double z) { this.z = z; } 
+    public void setName(String name) { this.name = name; }
     
 }
