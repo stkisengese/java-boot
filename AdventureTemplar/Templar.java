33420344be1bf119ca27f97fcec8aef7c5580cd5
Stephen Kisengese <skisengese@gmail.com>
2025-11-15 00:46:01 +0300

    feat: implement Templar class with healing and shielding capabilities

---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,36 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    @Override
+    public void heal(Character target) {
+        target.setCurrentHealth(target.getCurrentHealth() + healCapacity);
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public int getShield() {
+        return shield;
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() > 0) {
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
+                    getName(), getCurrentHealth(), healCapacity, shield);
+        } else {
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP.",
+                    getName(), shield, healCapacity);
+        }
+    }
+}
