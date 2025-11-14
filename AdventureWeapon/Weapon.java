912cd863d05ede313458357a35626d46eed5d824
Stephen Kisengese <skisengese@gmail.com>
2025-11-15 01:22:42 +0300

    feat: add new class Weapon and its methods

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -1,3 +1,22 @@
 public class Weapon {
-    
+    private final String name;
+    private final int damage;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s deals %d damages", name, damage);
+    }
 }
