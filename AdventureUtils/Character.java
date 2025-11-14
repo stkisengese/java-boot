b871cfcb3f47b61b3f616cf924244edbb5536a16
Stephen Kisengese <skisengese@gmail.com>
2025-11-14 23:35:06 +0300

    feat: add character tracking and status printing functionality

--- AdventureUtils/Character.java
+++ AdventureUtils/Character.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
     private final int maxHealth;
     private int currentHealth;
     private final String name;
+    public static List<Character> allCharacters = new ArrayList<>();
 
     // parameterized constructor
     public Character(String name, int maxHealth) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
+        allCharacters.add(this);
     }
 
     public int getMaxHealth() {
@@ -39,11 +44,18 @@ public class Character {
         }
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Character other = (Character) obj;
-        return this.name.equals(other.name) && this.maxHealth == other.maxHealth;
+       public static String printStatus() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("------------------------------------------\n");
+        if (allCharacters.isEmpty()) {
+            sb.append("Nobody's fighting right now !\n");
+        } else {
+            sb.append("Characters currently fighting :\n");
+            for (Character c : allCharacters) {
+                sb.append(" - ").append(c.toString()).append("\n");
+            }
+        }
+        sb.append("------------------------------------------\n");
+        return sb.toString();
     }
 }
