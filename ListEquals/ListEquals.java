8d76c4898c2d69620c7e60fc988a2aeee8c0cf4a
Stephen Kisengese <skisengese@gmail.com>
2025-11-16 05:58:53 +0300

    feat: and check to list equals

---
+++ ListEquals/ListEquals.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class ListEquals {
+    public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        // if (list1.size() != list2.size()) {
+        //     return false;
+        // }
+        // for (int i = 0; i < list1.size(); i++) {
+        //     if (!list1.get(i).equals(list2.get(i))) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        return list1.equals(list2);
+
+    }
+}
