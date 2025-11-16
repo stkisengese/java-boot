2ee52e2147ee1b01347bbb33639a7545275e0a12
Stephen Kisengese <skisengese@gmail.com>
2025-11-16 20:34:12 +0300

    feat: add map search

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        if (inventory == null || productId == null) {
+	    return -1;	
+	}
+	return inventory.getOrDefault(productId, -1);
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        List<String> result = new ArrayList<>();
+	if (inventory == null) {
+	    return result;
+	}
+
+	for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+	    if (entry.getValue() == price) {
+	    	result.add(entry.getKey());
+	    }
+	return result;
+	}
+    }
+}
