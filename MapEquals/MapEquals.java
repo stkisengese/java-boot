d7093392d0406dfaf8cb476df78467f7c04b2e52
Stephen Kisengese <skisengese@gmail.com>
2025-11-16 20:47:13 +0300

    feat: check map comparisson

---
+++ MapEquals/MapEquals.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class MapEquals {
+    public static boolean areMapsEqual(Map<String, Integer> map1, Map<String, Integer> map2) {
+        if (map1 == null && map2 == null) {
+            return true;
+        }
+        if (map1 == null || map2 == null) {
+            return false;
+        }
+        return map1.equals(map2);
+    }
+}
