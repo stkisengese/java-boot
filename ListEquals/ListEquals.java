e931f8c11906cc9b75c574057772ad489d2661ed
Stephen Kisengese <skisengese@gmail.com>
2025-11-16 06:05:46 +0300

    feat: add check for null lists

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        if (list1 == null || list2 == null) {
+            return list1 == list2;
+        }
+
         // if (list1.size() != list2.size()) {
         //     return false;
         // }
