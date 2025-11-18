60f965744682a59247b6bfeb4ca34a1af73c9bbf
Stephen Kisengese <skisengese@gmail.com>
2025-11-18 16:59:21 +0300

    feat: add reduce method

---
+++ StreamReduce/StreamReduce.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamReduce {
+    public static Integer sumAll(Stream<Integer> s) {
+        return s.reduce(0, Integer::sum);
+    }
+
+    public static Integer divideAndAddElements(Stream<Integer> s, int divider) {
+        return s.reduce(0, (acc, num) -> acc + num / divider);
+    }
+}
