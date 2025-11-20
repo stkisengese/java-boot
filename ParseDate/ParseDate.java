a5fdaaf5694034c40bf5a36ca81c45f31dda40c9
Stephen Kisengese <skisengese@gmail.com>
2025-11-20 16:20:14 +0300

    feat: handle empty or null inputs

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -8,16 +8,24 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        }
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH);
         return LocalDate.parse(stringDate, formatter);
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
-           // Example: "09 heures du soir, 07 minutes et 23 secondes" -> Custom parsing
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        // Example: "09 heures du soir, 07 minutes et 23 secondes" -> Custom parsing
         String[] parts = stringDate.split(", ");
         int hours = Integer.parseInt(parts[0].split(" ")[0]);
         int minutes = Integer.parseInt(parts[1].split(" ")[0]);
