5d8e321953cc6377db4243591ac3766a3390d601
Stephen Kisengese <skisengese@gmail.com>
2025-11-20 16:46:55 +0300

    feat: handle null values

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -6,18 +6,29 @@ import java.util.Locale;
 
 public class FormatDate {
     public static String formatToFullText(LocalDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'Le' d MMMM ''de l'an' yyyy 'à' HH'h'mm'm' 'et' ss's'", Locale.FRENCH);
+        if (dateTime == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+            "'Le' d MMMM ''de l'an'' yyyy 'à' HH'h'mm'm' 'et' ss's'",
+            Locale.FRENCH
+        );
         return dateTime.format(formatter);
     }
 
     public static String formatSimple(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
         return date.format(formatter);
     }
 
     public static String formatIso(LocalTime time) {
+        if (time == null) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSS");
         return time.format(formatter);
     }
 }
-
