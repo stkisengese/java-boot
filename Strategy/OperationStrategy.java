4db4a11fe22ae6506765b8040548f0d17e1b9bea
Stephen Kisengese <skisengese@gmail.com>
2025-11-24 14:50:29 +0300

    fix: set class and method as abstract

--- Strategy/OperationStrategy.java
+++ Strategy/OperationStrategy.java
@@ -1,7 +1,5 @@
 // package Strategy;
 
-public class OperationStrategy {
-    public int execute(int a, int b) {
-        return a + b;
-    }
+public abstract class OperationStrategy {
+    public abstract int execute(int a, int b);
 }
