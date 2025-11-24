76fbdaec048ae1e84f2a8823e5f7089f898f6a4d
Stephen Kisengese <skisengese@gmail.com>
2025-11-24 14:11:19 +0300

    feat: implement Raclette interface in RacletteDecorator class

--- Decorator/RacletteDecorator.java
+++ Decorator/RacletteDecorator.java
@@ -1,17 +1,24 @@
 // package Decorator;
 
-public abstract class RacletteDecorator {
+public abstract class RacletteDecorator implements Raclette {
     private Raclette raclette;
 
     public RacletteDecorator(Raclette raclette) {
          this.raclette = raclette;
     }
 
+    @Override
     public int getCalories() {
         return raclette.getCalories();
     }
 
+    @Override
     public String getIngredients() {
         return raclette.getIngredients();
+    }
+
+    @Override
+    public String toString() {
+        return  getIngredients() + " pour " + getCalories() + " calories";
     }
 }
